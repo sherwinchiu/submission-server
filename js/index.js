9f2733e7825f587b0c0986b124112b176e671bb6
Sherwin Chiu <sherwin.chiu89@gmail.com>
2022-07-22 19:54:56 -0400

    index.js: add variable submission tests

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const spawn = require("child_process").spawn;
 
 var port = 5500;
-var submission_number = 0;
+var submission_number = 1;
 var directory = "../exercise-scripts/week1/question1/answer" + submission_number + ".cpp";
 var python_file = "week1-question1.py";
 
@@ -24,21 +24,22 @@ app.get("/", function (req, res) {
 });
 
 io.on("connection", (socket) => {
-    socket.on("upload", (file, callback) => {
-        directory = "../exercise-scripts/week1/question1/answer" + submission_number + ".cpp";
+    socket.on("upload", (file, week_num, question_num, callback) => {
         try {
+            python_file = "week" + week_num + "-question" + question_num + ".py";
+            directory = "../exercise-scripts/week" + week_num + "/question" + question_num + "/answer" + submission_number + ".cpp";
             fs.writeFile(path.join(__dirname, directory), file.toString(), function (err) {
                 if (err) {
                     console.log(err);
                 } else {
                     console.log("File " + submission_number.toString() + " written.");
                 }
+                submission_number++;
             });
             var process = spawn("python", [path.join(path.join("submission_server", directory), "../" + python_file), submission_number.toString()]);
             process.stdout.on("data", function (data) {
                 console.log(data.toString());
             });
-            submission_number++;
         } catch (err) {
             console.error(err);
         }
