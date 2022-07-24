bfcb7057104115a42b0e704aa5124b1f7a9f7a2c
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-07-24 16:29:23 -0400

    buttons display questions

--- js/client.js
+++ js/client.js
@@ -1,11 +1,14 @@
 const socket = io();
+var question = document.getElementById("current-question");
 
 function upload(files, week_num, question_num) {
     socket.emit("upload", files[0], week_num, question_num, (status) => {
         console.log(status);
     });
 }
-function change_text(question_num) {}
+function change_text(question_num) {
+    question.innerHTML = questions[question_num - 1];
+}
 
 // socket.on(""){
 
--- js/index.js
+++ js/index.js
@@ -14,7 +14,17 @@ var port = 5500;
 var submission_number = 1;
 var directory = "../exercise-scripts/week1/question1/answer" + submission_number + ".cpp";
 var python_file = "week1-question1.py";
-var question = "./exercise-scripts/week1/question1/tests.md";
+var question = "./exercise-scripts/week1/week1-tests.md";
+var question_paths = [
+    "./exercise-scripts/week1/question1/tests.md",
+    "./exercise-scripts/week1/question2/tests.md",
+    "./exercise-scripts/week1/question3/tests.md",
+    "./exercise-scripts/week1/question4/tests.md",
+    "./exercise-scripts/week1/question5/tests.md",
+    "./exercise-scripts/week1/question6/tests.md",
+    "./exercise-scripts/week1/question7/tests.md",
+    "./exercise-scripts/week1/question8/tests.md"
+]
 
 server.listen(port, function () {
     console.log("Server started on port 5500");
@@ -26,7 +36,11 @@ app.get("/", function (req, res) {
     try {
         let index = fs.readFileSync("./templates/index.html", "utf-8");
         let markdown = fs.readFileSync(question, "utf-8");
-        res.send(index.replace("{{ MD_CONTENT }}", md.render(markdown)));
+        let question_content = [];
+        for (let i = 0;i < question_paths.length;i++) {
+            question_content.push(md.render(fs.readFileSync(question_paths[i], "utf-8")));
+        }
+        res.send(index.replace("{{ MD_CONTENT }}", md.render(markdown)).replace("{{ QUESTION_CONTENT }}", JSON.stringify(question_content)));
     } catch (err) {
         console.error(err);
     }
